61deb91515d6d8fff7fcbfa366be7ce70e640e64
TakeNap <dev.gymnastics1960@gmail.com>
2023-03-28 14:01:35 +0900

    Make web site, improve js file to interact with contract

--- js/Aizu_Muzin.js
+++ js/Aizu_Muzin.js
@@ -1,5 +1,6 @@
 //Info of Aizumujin balance and token contract address
 let tokenAddress = "0x4841E1206065221d89935Ae2983EB7682D95D270";
+//[ISSUE]faucetのアドレスに変更する必要あり。2023/03/28米田
 let walletAddress = "0xCC106Fd59Ce735E3f510fbA9b1C369752f25bBD4";
 
 window.onload = function() {
@@ -8,11 +9,15 @@ window.onload = function() {
     } else {
         web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io"));
     }
+    
+    //initializing
     //display the balance of AizuMujin
-    getERC20TokenBalance();
+    getERC20TokenBalance(walletAddress,"result");
+    //the balance of the user now access here
 }
 //get the token balance of AizuMujin
-function getERC20TokenBalance() {
+//elm means which element you want to display on
+function getERC20TokenBalance(_walletAddress,elm) {
     let balance;
     // ERC20 トークンの残高を取得するための最小限のABI
     let minABI = [
@@ -34,53 +39,34 @@ function getERC20TokenBalance() {
         }
     ];
 
-    //  ABI とコントラクト（ERC20トークン）のアドレスから、コントラクトのインスタンスを取得 
-    let contract = web3.eth.contract(minABI).at(tokenAddress);
-    // 引数にウォレットのアドレスを渡して、balanceOf 関数を呼ぶ
-    contract.balanceOf(walletAddress, (error, balance) => {
+        //  ABI とコントラクト（ERC20トークン）のアドレスから、コントラクトのインスタンスを取得 
+        let contract = web3.eth.contract(minABI).at(tokenAddress);
+        // 引数にウォレットのアドレスを渡して、balanceOf 関数を呼ぶ
+        contract.balanceOf(_walletAddress, (error, balance) => {
         // ERC20トークンの decimals を取得
         contract.decimals((error, decimals) => {
         // 残高を計算
         balance = balance.div(10**decimals);
         console.log(balance.toString());
         //display balance of Aizumujin
-        document.getElementById('result').innerText = balance.toString();
+        document.getElementById(elm).innerText = balance.toString();
         });
     });
-
 }
-
 
 function getERC20TokenContract(tokenAddress) {
-let minABI = [
-    {"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"type":"function"},
-    {"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"type":"function"},
-    {"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"type":"function"}
-];
-return web3.eth.contract(minABI).at(tokenAddress);
+    let minABI = [
+        {"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"type":"function"},
+        {"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"type":"function"},
+        {"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"type":"function"}
+    ];
+    return web3.eth.contract(minABI).at(tokenAddress);
 }
 
-function getERC20TokenDecimals(callback) {
-    window.tokenContract.decimals((error, decimals) => {
-        callback(decimals);
-    });
-}
-
-function transferERC20Token(toAddress, value, callback) {
-    window.tokenContract.transfer(toAddress, value, (error, txHash) => {
-        alert(txHash);
-        callback(txHash);
-    });
-}
-
-function send() {
-    let toAddress = walletAddress;
-    let decimals = web3.toBigNumber(10);
-    let amount = web3.toBigNumber(document.getElementById('price').value);
-    let sendValue = amount.times(web3.toBigNumber(10).pow(18));
-    console.log(sendValue.toString());
-    transferERC20Token(toAddress, sendValue, (txHash) => {
-        document.getElementById('transaction').innerText = txHash;
-    });
-}
-
+function onAddressChange(e) { 
+    let tokenAddress = document.getElementById('myAddress').value; 
+    if(tokenAddress != "") 
+    { 
+        getERC20TokenBalance(tokenAddress, "result2");
+    }
+} 
